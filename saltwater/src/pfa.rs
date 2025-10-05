c4715a1df5b8711c7dbca66c1d3cd5770353b2ce
Tanika Mellifont-Young <tanika.young@proton.me>
2025-10-05 19:20:16 +1100

    added page frame deallocation

--- saltwater/src/pfa.rs
+++ saltwater/src/pfa.rs
@@ -1,9 +1,13 @@
 use crate::mapping::physical_to_virtual_address;
-use alloc::{boxed::Box, vec::Vec};
-use core::slice;
+use alloc::vec::Vec;
+use core::{slice, u8};
 use spinning_top::Spinlock;
 pub const PAGE_SIZE: usize = 4096;
-static PAGE_FRAME_BITMAP: Spinlock<Option<Box<&mut [u8]>>> = Spinlock::new(None);
+struct BitmapPageFrameAllocator {
+    bitmap: &'static mut [u8],
+    last_allocated_frame_index: usize,
+}
+static PAGE_FRAME_ALLOCATOR: Spinlock<Option<BitmapPageFrameAllocator>> = Spinlock::new(None);
 pub fn initialise(boot_info: &mut bootloader_api::BootInfo) {
     let bitmap_size: usize = boot_info
         .memory_regions
@@ -25,15 +29,14 @@ pub fn initialise(boot_info: &mut bootloader_api::BootInfo) {
             .expect("no memory regions were large enough to store page frame bitmap!")
             .start,
     );
-    PAGE_FRAME_BITMAP.lock().insert(Box::new(unsafe {
+    let mut allocator_guard = PAGE_FRAME_ALLOCATOR.lock();
+    let allocator = allocator_guard.insert(BitmapPageFrameAllocator { bitmap: unsafe {
         slice::from_raw_parts_mut(
             physical_to_virtual_address(bitmap_address).as_mut_ptr::<u8>(),
             bitmap_size,
         )
-    }));
-    let mut bitmap_guard = PAGE_FRAME_BITMAP.lock();
-    let bitmap = bitmap_guard.as_mut().expect("bitmap guard could not be acquired for page frame allocator initialisation!");
-    bitmap.fill(0);
+    }, last_allocated_frame_index: 0 });
+    allocator.bitmap.fill(u8::MAX);
     for memory_region in boot_info
         .memory_regions
         .iter()
@@ -41,10 +44,20 @@ pub fn initialise(boot_info: &mut bootloader_api::BootInfo) {
         .collect::<Vec<&bootloader_api::info::MemoryRegion>>()
     {
         for frame in (memory_region.start as usize / PAGE_SIZE)..((memory_region.end - 1) as usize / PAGE_SIZE) + 1 {
-            bitmap[frame / 8] &= !(1 << (frame % 8))
+            allocator.bitmap[frame / 8] &= !(1 << (frame % 8))
         }
     }
     for frame in (bitmap_address.as_u64() as usize / PAGE_SIZE)..(bitmap_address.as_u64() as usize + bitmap_size) + 1 {
-        bitmap[frame / 8] |= 1 << (frame % 8);
+        allocator.bitmap[frame / 8] |= 1 << (frame % 8);
+    }
+}
+struct PageFrame {
+    index: usize
+}
+impl Drop for PageFrame {
+    fn drop(&mut self) {
+        let mut allocator_guard = PAGE_FRAME_ALLOCATOR.lock();
+        let allocator = allocator_guard.as_mut().expect("page frame allocator not initialised before page frame deallocation attempted!");
+        allocator.bitmap[self.index / 8] &= !(1 << (self.index % 8));
     }
 }
