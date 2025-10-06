d7c6810df04d051ba686116223236b3921845d91
Tanika Mellifont-Young <tanika.young@proton.me>
2025-10-07 03:40:58 +1100

    added printing of page table entries at boot

--- saltwater/src/sysstacks.rs
+++ saltwater/src/sysstacks.rs
@@ -1,4 +1,3 @@
-use crate::pfa::{PageFrame};
 use crate::mapping::{self, PAGE_SIZE, SYSTEM_STACK_SIZE};
 #[repr(align(4096))]
 struct DoubleFaultStack {
@@ -10,7 +9,7 @@ pub static DOUBLE_FAULT_STACK: DoubleFaultStack = DoubleFaultStack {
 pub fn initialise(_boot_info: &mut bootloader_api::BootInfo) {
     for processor_index in 1..crate::acpi::PROCESSOR_COUNT.lock().expect("processors not counted before allocation of system stacks!") {
         for frame_index in 0..(SYSTEM_STACK_SIZE / PAGE_SIZE) {
-            mapping::system_stack_virtual_address(processor_index)
+            mapping::system_stack_virtual_address(processor_index);
         }
     }
 }
