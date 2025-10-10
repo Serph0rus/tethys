b1f18dc9cc891d666512549ff8614e5dd8cf6eec
Serph0rus <163723186+Serph0rus@users.noreply.github.com>
2025-10-11 02:40:21 +1100

    update main.rs to change "system" to "kernel" and disable initialisers for files not yet rewriten for 0.3.0

--- saltwater/src/main.rs
+++ saltwater/src/main.rs
@@ -1,32 +1,32 @@
 #![no_std]
 #![no_main]
 pub extern crate alloc;
-mod page;
-mod config;
-mod qemu;
-mod acpi;
-mod allocator;
-mod debugcon;
 mod gdt;
 mod hcf;
+mod debugcon;
+mod port;
 mod mapping;
+mod config;
+mod page;
 mod panic;
-mod pfa;
-mod sysstacks;
+mod qemu;
+mod allocator;
+mod acpi;
+mod frame;
 use crate::hcf::hcf;
-const INITIALISERS: [fn(&mut bootloader_api::BootInfo); 7] = [
+const INITIALISERS: [fn(&mut bootloader_api::BootInfo); 5] = [
     mapping::initialise,
     gdt::bootstrap_initialise,
     allocator::bootstrap_initialise,
     acpi::bootstrap_initialise,
-    pfa::initialise,
-    sysstacks::initialise,
-    gdt::initialise,
+    frame::initialise,
+    // stacks::initialise,
+    // gdt::initialise
 ];
 bootloader_api::entry_point!(main, config = &config::BOOTLOADER_CONFIG);
 pub fn main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     println!(
-        "exited from rust bootloader {} version {}.{}.{}, entering saltwater tethys system at address 0x{:x}...",
+        "exited from rust bootloader {} version {}.{}.{}, entering saltwater tethys kernel at address 0x{:x}...",
         if boot_info.api_version.pre_release() {
             "pre-release"
         } else {
@@ -40,6 +40,7 @@ pub fn main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
     for initialiser in INITIALISERS {
         initialiser(boot_info);
     }
-    println!("successfully initialised saltwater tethys system!");
+    qemu::exit(qemu::ExitCode::Success);
+    println!("successfully initialised saltwater tethys kernel!");
     hcf();
 }
