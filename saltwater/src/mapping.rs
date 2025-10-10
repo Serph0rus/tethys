ea32c3d2242b1cbfdf379d35ab22468e06e75aa0
Serph0rus <163723186+Serph0rus@users.noreply.github.com>
2025-10-11 02:41:11 +1100

    new stack mapping scheme + functions

--- saltwater/src/mapping.rs
+++ saltwater/src/mapping.rs
@@ -3,14 +3,20 @@ const SIXTEEN_MEGABYTES: u64 = 0x0000_0000_0100_0000;
 const SIXTEEN_TERABYTES: u64 = 0x0000_1000_0000_0000;
 pub const PAGE_SIZE: u64 = 4096;
 pub const HIGHER_HALF: u64 = 0xffff_8000_0000_0000;
-pub const SYSTEM_CODE: u64 = HIGHER_HALF;
-pub const SYSTEM_HEAP: u64 = SYSTEM_CODE + SIXTEEN_TERABYTES;
-pub const FRAMEBUFFER: u64 = SYSTEM_HEAP + SIXTEEN_TERABYTES;
-pub const SYSTEM_STACKS: u64 = FRAMEBUFFER + SIXTEEN_TERABYTES;
-pub const DIRECT_PHYSICAL: u64 = SYSTEM_STACKS + SIXTEEN_TERABYTES;
-pub const SYSTEM_STACK_SIZE: u64 = SIXTEEN_MEGABYTES;
-pub const fn system_stack_virtual_address(index: usize) -> u64 {
-    SYSTEM_STACKS + index as u64 * SYSTEM_STACK_SIZE
+pub const KERNEL_CODE: u64 = HIGHER_HALF;
+pub const KERNEL_HEAP: u64 = KERNEL_CODE + SIXTEEN_TERABYTES;
+pub const BOOTSTRAP_STACK: u64 = KERNEL_HEAP + SIXTEEN_TERABYTES;
+pub const FRAMEBUFFER: u64 = BOOTSTRAP_STACK + SIXTEEN_TERABYTES;
+pub const INTERRUPT_STACKS: u64 = FRAMEBUFFER + SIXTEEN_TERABYTES;
+pub const KERNEL_STACKS: u64 = INTERRUPT_STACKS + SIXTEEN_TERABYTES;
+pub const DIRECT_PHYSICAL: u64 = KERNEL_STACKS + SIXTEEN_TERABYTES;
+pub const INTERRUPT_STACK_SIZE: u64 = SIXTEEN_MEGABYTES;
+pub const KERNEL_STACK_SIZE: u64 = SIXTEEN_MEGABYTES;
+pub const fn interrupt_stack_address(index: usize) -> u64 {
+    KERNEL_STACKS + (2 * index as u64) * INTERRUPT_STACK_SIZE
+}
+pub const fn double_fault_stack_address(index: usize) -> u64 {
+    interrupt_stack_address(index) + INTERRUPT_STACK_SIZE
 }
 pub const fn physical_to_virtual_address(physical: u64) -> u64 {
     DIRECT_PHYSICAL + physical
@@ -23,3 +29,4 @@ pub fn initialise(boot_info: &mut bootloader_api::BootInfo) {
     });
     println!("mapped physical memory at address 0x{:x}...", boot_info.physical_memory_offset.into_option().expect("bootloader did not map physical memory!"));
 }
+// need to dynamically allocate a kernel stack for each process, and an interrupt stack + double fault stack for each processor
