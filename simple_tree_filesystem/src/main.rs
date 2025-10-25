f3e12fca1652e24bdac19a189c63f6966807b5ba
Tanika Mellifont-Young <tanika.young@proton.me>
2025-10-25 19:48:28 +1100

    added simple tree filesystem

--- simple_tree_filesystem/src/main.rs
+++ simple_tree_filesystem/src/main.rs
@@ -1,7 +1,16 @@
+#![no_std]
+extern crate alloc;
 use spinning_top::Spinlock;
+use hashbrown::{DefaultHashBuilder, HashMap};
 const SIZE: usize = 3500;
 const BLOCK_SIZE: usize = 4096;
 static PARTITION: Spinlock<[u8; SIZE * BLOCK_SIZE]> = Spinlock::new([0; SIZE * BLOCK_SIZE]);
+struct DescriptorData {
+
+}
+lazy_static! {
+    static ref DESCRIPTOR_DATA: HashMap<usize, DescriptorData> = HashMap::new();
+}
 pub fn main() {
-    
+
 }
