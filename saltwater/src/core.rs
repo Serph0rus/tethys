6055896208f0ad191be812b23c50f7a22be8f481
Tanika Mellifont-Young <tanika.young@proton.me>
2025-11-19 22:11:43 +1100

    added core.rs for processor-local data and initialisation

---
+++ saltwater/src/core.rs
@@ -0,0 +1,15 @@
+use alloc::format;
+use x86_64::structures::gdt::GlobalDescriptorTable;
+use crate::gdt::{self, Selectors};
+pub struct ProcessorData {
+    pub gdt: &'static GlobalDescriptorTable,
+    pub selectors: Selectors,
+}
+pub unsafe extern "C" fn initialise(processor: usize) -> () {
+    let gdt_selectors = gdt::GLOBAL_DESCRIPTOR_TABLES.lock().pop().expect(&format!("processor {} could not find global descriptor table!", processor));
+    let data = ProcessorData {
+        gdt: gdt_selectors.0,
+        selectors: gdt_selectors.1,
+    };
+    unsafe {gdt::load(data.gdt, data.selectors)};
+}
